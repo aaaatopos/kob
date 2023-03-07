1ffcbe38c6b646801623b84d8e5825c1366cc8c6
Xie Zetao <xzt2520@163.com>
2023-03-07 17:01:29 +0800

    项目初步搭建完成

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## King Of Bots
+
+后端：使用SpringBoot
+
+数据库：使用MySQL
+
+前端：使用Vue，分别实现两个前端：Web端和AcApp端
+
--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,7 +1,12 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.lang.model.element.NestingKind;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author xzt
@@ -10,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pk")
 public class BotInfoController {
+
+    @GetMapping("/getinfo")
+    public Map<String, String> getBotInfo(){
+        Map<String,String> bot = new HashMap<>();
+        bot.put("name", "bot");
+        bot.put("rating", "1500");
+        return bot;
+    }
 }
